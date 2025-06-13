11f1f22332571e383d9d77e23598751256df08e8
Kyczar Aalbregtse <kyczaraalbregtse@gmail.com>
2025-06-13 16:54:29 -0400

    fixed unused tesimonials

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import HeroSection from '../components/banners/hero-section';
 import '../styles/page-styles/home.css';
 import Process from '../components/informative/process';
-import Testimonials from '../components/informative/testomonials';
 import PortfolioCarousel from '../components/informative/portfolio-carousel';
 import WhyChooseUs from '../components/informative/why-choose-us';
 import FiftyFiftyPortfolio from '../components/informative/fifty-fifty-portfolio';
