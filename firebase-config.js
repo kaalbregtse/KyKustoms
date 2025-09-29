4db92470190363ab8c8e8927ef8b156b36811051
Kyczar Aalbregtse <106981776+kaalbregtse@users.noreply.github.com>
2025-09-29 11:52:23 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIobjFI9UVZLoE_zKb0Zf-i5WOR9RTz2E",
-  authDomain: "kykustoms-feddb.firebaseapp.com",
-  projectId: "kykustoms-feddb",
-  storageBucket: "kykustoms-feddb.firebasestorage.app",
-  messagingSenderId: "652926314369",
-  appId: "1:652926314369:web:fd194f0cb36f639decea80",
-  measurementId: "G-4QQR8Q7WPB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
